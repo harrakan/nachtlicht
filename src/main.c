f79dbe290ece0a2d5760d56da31b50a0d44cee24
Harrakan <a@b.c>
2015-09-11 18:19:54 +0000

    changed to nachtlicht progam

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 #include <avr/interrupt.h>
 #include "light_ws2812.h"
 
-//#define F_CPU 8000000UL         /* 8MHz crystal oscillator */
+#define F_CPU 8000000UL         /* 8MHz crystal oscillator */
 
 #define BUTTON_PORT PORTB       /* PORTx - register for button output */
 #define BUTTON_PIN PINB         /* PINx - register for button input */
@@ -22,25 +22,35 @@
 #define LOCK_INPUT_TIME 250     /* time to wait after a button press */
 
 #define  StripLength      2
+#define  Delay_time_ms    20
+#define  Button_count    10
 
 struct cRGB led[StripLength];
-
 struct Color {  uint8_t red; uint8_t green; uint8_t blue; };
 
-int buttonstate = 0;
+int button_pressed_counter;
+int dimmer_counter;
+struct Color global_color = {0,0,0};
+unsigned char brigtness;
 
+struct Color black = {0,0,0};    
+struct Color red = {255,0,0};  
+struct Color white = {255,255,255};
 
-void setColor(struct Color color, int from, int to) 
+void setColor(struct Color color, int from, int to, unsigned char brightness) 
 {
     
     for (int i = 0; i <= StripLength; i++) {
         if ( i >= from &&  i < to )
         {
-            led[i].r=color.green;
-            led[i].g=color.red;
-            //led[i].r=color.red;
-            //led[i].g=color.green;
-            led[i].b=color.blue;  
+            led[i].r=color.green * brightness/255.0;
+            led[i].g=color.red * brightness/255.0;
+            /*
+             * original colors; depends on LEDs
+            led[i].r=color.red * brightness/255;
+            led[i].g=color.green * brightness/255;
+            */
+            led[i].b=color.blue * brightness/255.0;  
         }
         else
         {
@@ -52,9 +62,9 @@ void setColor(struct Color color, int from, int to)
     ws2812_setleds(led,StripLength);
 }
 
-void init_io() 
+void init() 
 {
-   
+    
     CLKPR=_BV(CLKPCE);
     CLKPR=0;			// set clock prescaler to 1 (attiny 25/45/85/24/44/84/13/13A)
     
@@ -64,59 +74,70 @@ void init_io()
     /* turn on internal pull-up resistor for the switch */
     BUTTON_PORT |= _BV(BUTTON_BIT);
     
-
-    struct Color black = {0,0,0};
-    setColor(black ,0,StripLength);
+    
+    setColor(black ,0,StripLength,0);
 }
 
 int button_is_pressed()
 {
-        /* the button is pressed when BUTTON_BIT is clear */
-        if (bit_is_clear(BUTTON_PIN, BUTTON_BIT))
-        {
-                _delay_ms(DEBOUNCE_TIME);
-                if (bit_is_clear(BUTTON_PIN, BUTTON_BIT)) return 1;
-        }
-
-        return 0;
+    /* the button is pressed when BUTTON_BIT is clear */
+    if (bit_is_clear(BUTTON_PIN, BUTTON_BIT))
+    {
+        _delay_ms(DEBOUNCE_TIME);
+        if (bit_is_clear(BUTTON_PIN, BUTTON_BIT)) return 1;
+    }
+    
+    return 0;
 }
 
 
-int main(void)
+void loop()
 {
-    
-    init_io();
+  if (button_is_pressed() )
+  {
+    button_pressed_counter ++;
+  }
+  else
+  {if (button_pressed_counter == 0 ) // no button
+    {
+      dimmer_counter ++;
+      if (brigtness > 0 && dimmer_counter > 10) {
+      brigtness--;
+      dimmer_counter  =0;      
+      setColor(global_color, 0, StripLength, brigtness);
+      }
+    }
+    else
+    {
+      if (button_pressed_counter < Button_count)
+      {
+        if (brigtness == 0) {
+          brigtness = 200;
+          global_color = red;
+        }
+        else {
+          brigtness = 0;
+        }
+      }
+      else
+      {
+        global_color = white;
+        brigtness = 200;
+      }
+      button_pressed_counter = 0;
+      setColor(global_color, 0, StripLength, brigtness);
+    }
+  }
+  _delay_ms(Delay_time_ms);
+}
+      
+
+int main(void)
+{    
+    init();
     
     while(1)
     {
-        if (button_is_pressed())
-        {
-            struct Color rgb;
-            rgb.red = 255;
-            rgb.green = 0;
-            rgb.blue = 0;
-            setColor(rgb,0,StripLength);
-            _delay_ms(500);                         // wait for 500ms.
-            
-            rgb.red = 0;
-            rgb.green = 0;
-            rgb.blue = 255;
-            setColor(rgb,1,2);
-            _delay_ms(500);
-            
-            rgb.red = 0;
-            rgb.green = 255;
-            rgb.blue = 0;
-            setColor(rgb,0,1);
-            _delay_ms(500);
-            
-            
-            rgb.red = 0;
-            rgb.green = 0;
-            rgb.blue = 0;
-            setColor(rgb,0,StripLength);
-            _delay_ms(500);
-        }
-        
+        loop();               
     }
 }
